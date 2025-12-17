e9d95e976cb8c247621371a4a0dcf7e104a490aa
Avery McLean <mcleanavery04@gmail.com>
2025-12-17 17:06:33 -0500

    User can reset board and choose dimension.

--- javascript.js
+++ javascript.js
@@ -10,3 +10,30 @@ for (i = 0; i < 256; i++) {
     square.classList.add("grid-square");
     container.appendChild(square);
 }
+
+function clear() {
+    container.innerHTML = '';
+}
+
+function resetGrid(size) {
+    for (let i = 0; i < size*size; i++) {
+        const square = document.createElement("div");
+        square.addEventListener("mouseenter", () => {
+            square.classList.add("hovered");
+        });
+        const squareSize = 512 / size;
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`
+
+        square.classList.add("grid-square");
+        container.appendChild(square);
+    }
+}
+
+const button = document.querySelector("#reset");
+
+button.addEventListener("click", () => {
+    let newSize = parseInt(prompt("Enter a new dimension for the grid."));
+    clear();
+    resetGrid(newSize);
+});
